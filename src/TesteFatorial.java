cdcc8d8c1c31de5731715c73fcf68232dbc48699
Leandro <leandropremier@gmail.com>
2018-04-26 21:05:49 -0300

    Comentario inserido em TesteFatorial.Java

--- src/TesteFatorial.java
+++ src/TesteFatorial.java
@@ -1,6 +1,6 @@
 
 public class TesteFatorial {
-	
+	//Comentario inserido em teste fatorial.java
 	public static void main (String args[]) {
 		Fatorial fat = new Fatorial();
 		int resultado = fat.calcularFatorial(0);
